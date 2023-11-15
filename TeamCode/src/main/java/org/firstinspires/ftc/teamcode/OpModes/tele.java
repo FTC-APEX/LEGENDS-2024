74cf9b3c992083322730dcf7864008f8314c9cee
smhuang <smhuang@exeter.edu>
2023-11-15 12:40:38 -0500

    Feat[Subsystems] Mechanum Wheels!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/tele.java
@@ -5,6 +5,8 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.slides;
@@ -12,9 +14,10 @@ import org.firstinspires.ftc.teamcode.subsystems.hang;
 import org.firstinspires.ftc.teamcode.subsystems.shooter;
 import org.firstinspires.ftc.teamcode.subsystems.intake;
 import org.firstinspires.ftc.teamcode.subsystems.outake;
+import org.firstinspires.ftc.teamcode.subsystems.mechanum;
 
 @TeleOp(name = "tele")
-public class tele extends LinearOpMode{
+public class tele extends LinearOpMode {
     //Input all subsystems & drivetrain
     SampleMecanumDrive drivetrain;
     slides Slides;
@@ -22,14 +25,19 @@ public class tele extends LinearOpMode{
     intake Sweep;
     shooter Shooter;
     hang Hanger;
-
-
-
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
 
 
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
 
         //Initialization of all Subsystems
@@ -42,19 +50,63 @@ public class tele extends LinearOpMode{
         Servo Claw = null;
         Servo Shooter = null;
 
+
         Claw = hardwareMap.get(Servo.class, "Claw");
         Shooter = hardwareMap.get(Servo.class, "Shooter");
 
         waitForStart();
         while (!isStopRequested()) {
-            while (opModeIsActive()){
+            while (opModeIsActive()) {
                 telemetry.addData("Robot", "Initialized");
+                telemetry.addData("Front Left: ", frontLeft.getCurrentPosition());
+                telemetry.addData("Front Right: ", frontRight.getCurrentPosition());
+                telemetry.addData("Back Left: ", backLeft.getCurrentPosition());
+                telemetry.addData("Back Right: ", backRight.getCurrentPosition());
+
+                while (!isStopRequested()) {
+                    // Translation
+                    if (Math.abs(gamepad1.left_stick_y) > 0.2 || Math.abs(gamepad1.left_stick_x) > 0.2 || Math.abs(gamepad1.right_stick_x) > 0.2) {
+                        frontLeft.setPower(((gamepad1.left_stick_y - gamepad1.right_stick_x * 1.22) - gamepad1.left_stick_x) / 1.2);
+                        frontRight.setPower(((gamepad1.left_stick_y * -1 - gamepad1.right_stick_x * 1.22) - gamepad1.left_stick_x) / 1.2);
+                        backRight.setPower(((gamepad1.left_stick_y + gamepad1.right_stick_x * 1.22) - gamepad1.left_stick_x) / 1.2);
+                        backLeft.setPower(((gamepad1.left_stick_y * -1 + gamepad1.right_stick_x * 1.22) - gamepad1.left_stick_x) / 1.2);
+                    } else {
+                        frontLeft.setPower(0);
+                        frontRight.setPower(0);
+                        backRight.setPower(0);
+                        backLeft.setPower(0);
+                    }
+
+                    if (gamepad1.dpad_up) { //dpad slow movement
+                        frontLeft.setPower(-0.3);
+                        frontRight.setPower(0.3);
+                        backRight.setPower(-0.3);
+                        backLeft.setPower(0.3);
+                    }
+                    if (gamepad1.dpad_right) {
+                        frontLeft.setPower(-0.5);
+                        frontRight.setPower(-0.5);
+                        backRight.setPower(-0.5);
+                        backLeft.setPower(-0.5);
+                    }
+                    if (gamepad1.dpad_left) {
+                        frontLeft.setPower(0.5);
+                        frontRight.setPower(0.5);
+                        backRight.setPower(0.5);
+                        backLeft.setPower(0.5);
+                    }
+                    if (gamepad1.dpad_down) {
+                        frontLeft.setPower(0.3);
+                        frontRight.setPower(-0.3);
+                        backRight.setPower(0.3);
+                        backLeft.setPower(-0.3);
+                    }
 
 
-
-                telemetry.update();
+                    telemetry.update();
+                }
+                drivetrain.update();
             }
-            drivetrain.update();
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/mechanum.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.*;
+
+//import com.qualcomm.robotcore.hardware.opmode;
+//import com.qualcomm.robotcore.hardware.opmode.Autonomous;
+
+public class mechanum {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+
+    public void init(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+    }
+
+
+}
