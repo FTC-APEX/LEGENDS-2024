fb8288f52628ba04a22bed87d0486ccb38a66bb8
Krish Kalla <116767392+Buddhimaan1026@users.noreply.github.com>
2024-01-06 23:16:04 -0500

    RED-BACK AUTONOMOUS PATHING DONE - NEEDS REVIEW * NO SUBSYSTEMS IN PLACE YET * ALL PATHING IS COMPLETE, BUT SOME CHECKS ARE NEEDED * ALL TEMPORAL & DISPLACEMENT MARKERS ABSENT * KAMERA CODE WILL BE RE-WRITTEN TO WORK WITH RED AND BLUE TOGETHER

--- MeepMeepTesting/src/main/java/com/apex/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/apex/meepmeeptesting/MeepMeepTesting.java
@@ -22,6 +22,7 @@ public class MeepMeepTesting {
                                         .lineToConstantHeading(new Vector2d(-36, -36))
                                         //For Zone 1
                                         .turn(Math.toRadians(90))
+                                        .waitSeconds(.5)
 
                                         .lineToConstantHeading(new Vector2d(48, -36))
                                         .waitSeconds(2)
@@ -67,12 +68,83 @@ public class MeepMeepTesting {
                                         .lineToConstantHeading(new Vector2d(60, -60))
 
                                         .build()
+                );
+
+        RoadRunnerBotEntity Bot2 = new DefaultBotBuilder(meepMeep)
+                // Assume starting
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setDimensions(16.5, 17.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, -60, Math.toRadians(90)))
+                                //Initial Movement
+
+                                //zone 1
+//                                .lineToConstantHeading(new Vector2d(-36,-48))
+//                                .splineTo(new Vector2d(-40, -32), Math.toRadians(125))
+//                                .waitSeconds(.5)
+
+                                //zone2
+                                .lineToConstantHeading(new Vector2d(-36, -34))
+
+
+                                //zone3
+//                                .lineToConstantHeading(new Vector2d(-36,-48))
+//                                .splineTo(new Vector2d(-32, -32), Math.toRadians(55))
+//                                .waitSeconds(.5)
+
+                                .setReversed(true)
+                                .splineTo(new Vector2d(-24, -36), Math.toRadians(0))
+                                .lineToConstantHeading(new Vector2d(48, -36))
+                                .setReversed(false)
+                                .waitSeconds(2)
+
+
+                                .splineTo(new Vector2d(0, -12), Math.toRadians(180))
+
+
+                                //.lineToConstantHeading(new Vector2d(48, -12))
+                                //.lineToConstantHeading(new Vector2d(62, -18))
+                                //.turn(Math.toRadians(180))
+                                .lineToConstantHeading(new Vector2d(-60, -12))
+
+                                .waitSeconds(1)
+                                .lineToConstantHeading(new Vector2d(0, -12))
+                                .splineTo(new Vector2d(48, -36), Math.toRadians(0))
+
+                                .waitSeconds(1)
+
+                                .splineTo(new Vector2d(0, -12), Math.toRadians(180))
+
+
+                                //.lineToConstantHeading(new Vector2d(48, -12))
+                                //.lineToConstantHeading(new Vector2d(62, -18))
+                                //.turn(Math.toRadians(180))
+                                .lineToConstantHeading(new Vector2d(-60, -12))
+
+                                .waitSeconds(1)
+                                .lineToConstantHeading(new Vector2d(0, -12))
+                                .splineTo(new Vector2d(48, -36), Math.toRadians(0))
+
+                                .waitSeconds(1)
+
+                                //.lineToConstantHeading(new Vector2d(48, -36))
+
+                                //park left
+//                                .lineToConstantHeading(new Vector2d(48, -12))
+//                                .lineToConstantHeading(new Vector2d(60, -12))
+                                //park right
+                                .lineToConstantHeading(new Vector2d(48, -60))
+                                .lineToConstantHeading(new Vector2d(60, -60))
+
+                                .build()
                 ); //Everyone should have turkeys to f
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(Bot2)
                 .start();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/constantsAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/constantsAutonomous.java
@@ -2,25 +2,23 @@ package org.firstinspires.ftc.teamcode.util;
 
 public class constantsAutonomous {
     public enum redFront {
-        CAMERA_TRAJECTORY,
-        TO_BACKDROP,
-        REVERSE_AND_PIVOT,
-        TO_STACK_INLINE_AND_PIVOT,
+        PURPLE_CAM,
+        SCORE_YELLOW,
         TO_STACK, // BOOK IT
-        BACK_TO_5TH_LANE_AND_PIVOT,
+        TO_BACKBOARD,
         STRAFE_TO_BACKDROP_INLINE,
         CYCLE_SCORE,
     }
 
     public enum redBack {
-        CAMERA_TRAJECTORY,
-        TO_BACKDROP,
-        REVERSE_AND_PIVOT,
-        TO_STACK_INLINE_AND_PIVOT,
+        IDLE,
+        PURPLE_CAM,
+        SCORE_YELLOW,
         TO_STACK, // BOOK IT
-        BACK_TO_5TH_LANE_AND_PIVOT,
-        STRAFE_TO_BACKDROP_INLINE,
-        CYCLE_SCORE,
+        TO_BACKBOARD,
+        PARK_LEFT,
+        PARK_RIGHT,
+        END
     }
 
     public enum blueFront {
