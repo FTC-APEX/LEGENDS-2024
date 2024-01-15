e66efe7107ad97f24a582f258858c922b3ec3ce5
Celine <celine.zhang2007@gmail.com>
2024-01-15 15:27:03 -0500

    wrote blue auto!!!

--- MeepMeepTesting/src/main/java/com/apex/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/apex/meepmeeptesting/MeepMeepTesting.java
@@ -3,6 +3,7 @@ package com.apex.meepmeeptesting;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -10,7 +11,7 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        /*RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Assume starting
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
@@ -68,74 +69,75 @@ public class MeepMeepTesting {
                                 .lineToConstantHeading(new Vector2d(60, -60))
 
                                 .build()
-                );
+                );*/
 
-        RoadRunnerBotEntity Bot2 = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity Bot2 = new DefaultBotBuilder(meepMeep) //blue side
                 // Assume starting
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
                 .setDimensions(16.5, 17.5)
                 .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(-36, -60, Math.toRadians(90)))
+                                drive.trajectorySequenceBuilder(new Pose2d(-36, 60, Math.toRadians(90)))
                                         //Initial Movement
 
                                         //zone 1
-//                                .lineToConstantHeading(new Vector2d(-36,-48))
-//                                .splineTo(new Vector2d(-40, -32), Math.toRadians(125))
+//                                .lineToConstantHeading(new Vector2d(-36,48))
+//                                .splineTo(new Vector2d(-40, 32), Math.toRadians(125))
 //                                .waitSeconds(.5)
 
                                         //zone2
-                                        .lineToConstantHeading(new Vector2d(-36, -34))
+                                        .lineToConstantHeading(new Vector2d(-36, 34))
 
 
                                         //zone3
-//                                .lineToConstantHeading(new Vector2d(-36,-48))
-//                                .splineTo(new Vector2d(-32, -32), Math.toRadians(55))
+//                                .lineToConstantHeading(new Vector2d(-36,48))
+//                                .splineTo(new Vector2d(-32, 32), Math.toRadians(55))
 //                                .waitSeconds(.5)
 
                                         .setReversed(true)
-                                        .splineTo(new Vector2d(-24, -36), Math.toRadians(0))
-                                        .lineToConstantHeading(new Vector2d(48, -36))
+                                        .splineTo(new Vector2d(-24, 36), Math.toRadians(0))
+                                        .lineToConstantHeading(new Vector2d(48, 36))
                                         .setReversed(false)
                                         .waitSeconds(2)
 
 
-                                        .splineTo(new Vector2d(0, -12), Math.toRadians(180))
+                                        .splineTo(new Vector2d(0, 12), Math.toRadians(180))
 
 
-                                        //.lineToConstantHeading(new Vector2d(48, -12))
-                                        //.lineToConstantHeading(new Vector2d(62, -18))
+                                        //.lineToConstantHeading(new Vector2d(48, 12))
+                                        //.lineToConstantHeading(new Vector2d(62, 18))
                                         //.turn(Math.toRadians(180))
-                                        .lineToConstantHeading(new Vector2d(-60, -12))
+                                        .lineToConstantHeading(new Vector2d(-60, 12))
 
                                         .waitSeconds(1)
-                                        .lineToConstantHeading(new Vector2d(0, -12))
-                                        .splineTo(new Vector2d(48, -36), Math.toRadians(0))
+                                        .lineToConstantHeading(new Vector2d(0, 12))
+                                        .splineTo(new Vector2d(48, 36), Math.toRadians(0))
 
                                         .waitSeconds(1)
 
-                                        .splineTo(new Vector2d(0, -12), Math.toRadians(180))
+                                        .splineTo(new Vector2d(0, 12), Math.toRadians(180))
 
 
-                                        //.lineToConstantHeading(new Vector2d(48, -12))
-                                        //.lineToConstantHeading(new Vector2d(62, -18))
+                                        //.lineToConstantHeading(new Vector2d(48, 12))
+                                        //.lineToConstantHeading(new Vector2d(62, 18))
                                         //.turn(Math.toRadians(180))
-                                        .lineToConstantHeading(new Vector2d(-60, -12))
+                                        .lineToConstantHeading(new Vector2d(-60, 12))
 
                                         .waitSeconds(1)
-                                        .lineToConstantHeading(new Vector2d(0, -12))
-                                        .splineTo(new Vector2d(48, -36), Math.toRadians(0))
+                                        .lineToConstantHeading(new Vector2d(0, 12))
+                                        .splineTo(new Vector2d(48, 36), Math.toRadians(0))
 
                                         .waitSeconds(1)
 
-                                        //.lineToConstantHeading(new Vector2d(48, -36))
+                                        //.lineToConstantHeading(new Vector2d(48, 36))
 
                                         //park left
-//                                .lineToConstantHeading(new Vector2d(48, -12))
-//                                .lineToConstantHeading(new Vector2d(60, -12))
+//                                .lineToConstantHeading(new Vector2d(48, 12))
+//                                .lineToConstantHeading(new Vector2d(60, 12))
                                         //park right
-                                        .lineToConstantHeading(new Vector2d(48, -60))
-                                        .lineToConstantHeading(new Vector2d(60, -60))
+                                        .lineToConstantHeading(new Vector2d(48, 60))
+                                        .lineToConstantHeading(new Vector2d(60, 60))
 
                                         .build()
                 ); //Everyone should have turkeys to f
@@ -143,7 +145,7 @@ public class MeepMeepTesting {
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                //.addEntity(myBot)
                 .addEntity(Bot2)
                 .start();
     }
