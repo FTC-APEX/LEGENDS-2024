51214ea37674e3fbb158716acad9c9bbe846de34
smhuang <smhuang@exeter.edu>
2024-01-23 20:22:22 -0500

    Parking Done finally

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/StandardTrackingWheelLocalizer.java
@@ -61,8 +61,6 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         if(FrontReversed) frontEncoder.setDirection(Encoder.Direction.REVERSE);
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
-        leftEncoder.setDirection(Encoder.Direction.REVERSE);
-        rightEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
