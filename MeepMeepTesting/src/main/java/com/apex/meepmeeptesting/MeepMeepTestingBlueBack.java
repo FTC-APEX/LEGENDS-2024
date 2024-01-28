e8215e58fd58a7cadb1f65278d951f7641d2a27e
Celine <celine.zhang2007@gmail.com>
2024-01-28 16:44:18 -0500

    3 meepmeep paths for yellow+purple red back and blue back

--- MeepMeepTesting/src/main/java/com/apex/meepmeeptesting/MeepMeepTestingBlueBack.java
+++ MeepMeepTesting/src/main/java/com/apex/meepmeeptesting/MeepMeepTestingBlueBack.java
@@ -4,6 +4,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -11,25 +12,37 @@ public class MeepMeepTestingBlueBack {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity Bot = new DefaultBotBuilder(meepMeep) //blue side away from stage
+        RoadRunnerBotEntity Bot = new DefaultBotBuilder(meepMeep) //red side away from stage
                 // Assume starting
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
                 .setDimensions(16.5, 17.5)
                 .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(-36, 60, Math.toRadians(90)))
+                                drive.trajectorySequenceBuilder(new Pose2d(-36, 60, Math.toRadians(-90)))
                                         //Initial Movement
 
-                                        //zone2
+//                                        //Left
+//                                        .splineTo(new Vector2d(-46.5, 38), Math.toRadians(-90))
+//                                        .setReversed(true)
+//                                        .splineTo(new Vector2d(-24, 36), Math.toRadians(0))
+//                                        .splineTo(new Vector2d(48, 29), Math.toRadians(0))
+//                                        .setReversed(false)
+//                                        .waitSeconds(2)
+                                        //Center
                                         .lineToConstantHeading(new Vector2d(-36, 34))
-
                                         .setReversed(true)
                                         .splineTo(new Vector2d(-24, 36), Math.toRadians(0))
                                         .lineToConstantHeading(new Vector2d(48, 36))
                                         .setReversed(false)
                                         .waitSeconds(2)
-
+//                                        //Right
+//                                        .splineTo(new Vector2d(-24, 36), Math.toRadians(-90))
+//                                        .setReversed(true)
+//                                        .splineTo(new Vector2d(-20, 36), Math.toRadians(0))
+//                                        .splineTo(new Vector2d(48, 43), Math.toRadians(0))
+//                                        .setReversed(false)
+//                                        .waitSeconds(2)
 
                                         .splineTo(new Vector2d(0, 12), Math.toRadians(180))
 
